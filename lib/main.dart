e1650ef468cae8734df9b961433b3981267e1520
banku27 <pankajparihar123321@gmail.com>
2022-09-17 15:44:36 +0530

    firebase setup and request permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:google_docs_clone/firebase_options.dart';
 import 'package:google_docs_clone/screens/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseMessaging.instance.getInitialMessage();
   runApp(const MyApp());
 }
 
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,6 +15,33 @@ class _MainScreenState extends State<MainScreen> {
   TextEditingController body = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+    requestPermission();
+  }
+
+  void requestPermission() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User Granted Permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User Granted Provisional Permission');
+    } else {
+      print('User declined or has not accepted the  permission');
+    }
+  }
+
+  @override
   void dispose() {
     username.dispose();
     title.dispose();
@@ -22,6 +51,46 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: username,
+            ),
+            TextFormField(
+              controller: title,
+            ),
+            TextFormField(
+              controller: body,
+            ),
+            GestureDetector(
+              onTap: () async {
+                String name = username.text.trim();
+                String titleText = title.text;
+                String bodyText = body.text;
+              },
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                height: 40,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.redAccent.withOpacity(0.5),
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text('Button'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
